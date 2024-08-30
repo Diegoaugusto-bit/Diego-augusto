bb7c608d682b47531e935987777e8eceac8483d0
Diegoaugusto-bit <diegoclash1407@gmail.com>
2024-08-30 14:27:34 -0300

    Add files via upload

---
+++ exercicio 2.js
@@ -0,0 +1,17 @@
+// crie uma variavel que receba e armazena a sua idade no tipo number.
+let idade = 17 
+//crie uma outra variavel que receba o valor da sua primeira variável,mas com o seu tipo alterado para string
+let idade2 = idade.toString()
+//3 imprima duas mensagens no terminal:
+//A primeira deve mostrar o valor e o tipo da primeira variável
+//A segunda deve mostrar o valor e o tipo da segunda variável
+console.log(idade, typeof idade)
+console.log(typeof idade2)
+
+console.log("ola, meu nome eh" ,idade, "e eu tenho", idade2, "anos")
+
+
+
+
+
+
