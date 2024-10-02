0ab4fd5ef9af63a34edfb48e547b3febd4948e12
Diegoaugusto-bit <diegoclash1407@gmail.com>
2024-10-02 14:25:01 -0300

    Update exerciciosCondicionais.js

--- exerciciosCondicionais.js
+++ exerciciosCondicionais.js
@@ -41,12 +41,16 @@ if (numero % 2 === 0) {
 
 /*
     a) Explique o que o código faz. Qual o teste que ele realiza? 
+     // o codigo esta mostrando se o numero e divisivel por 2 e o resultado e igual a 0
+
+
 
     b) Para que tipos de números ele imprime no console "Passou no teste"? 
+     // numeros pares
+
 
     c) Para que tipos de números a mensagem é "Não passou no teste"? 
-*/
-
+*/  // numeros impares
 
 //2. O código abaixo foi feito por uma pessoa desenvolvedora, contratada para automatizar algumas tarefas de um supermercado:
 
@@ -73,12 +77,16 @@ console.log(`O preço da fruta ${fruta} é R$ ${preco}`)
 
 /*
     a) Para que serve o código acima?
+    // para identificar o preco de cada fruta
+
 
     b) Qual será a mensagem impressa no console, se o valor de fruta for `"Maçã"`?
+     2.25
+
 
     c) Considere que um usuário queira comprar uma `Pêra`, qual seria a mensagem impressa no console se retirássemos o `break` que está logo acima 
     do `default` (o `break` indicado pelo comentário "BREAK PARA O ITEM c.")?
-*/
+*/  // o preço da fruta pera e 5
 
 
 //3. Leia o código abaixo:
@@ -93,8 +101,12 @@ console.log(mensagem)
 
 /*
     a) O que a primeira linha está fazendo?
+    criando uma constante que recebe o valor de 5
+
 
     b) Considere um usuário digitou o número 10. Qual será a mensagem do terminal? E se fosse o número -10?
+    daria erro do mesmo jeito
+
 
     c) Haverá algum erro no console? Justifique.
 */
@@ -127,17 +139,55 @@ console.log(mensagem)
 
 
 /*
-1. c
-*/
+1. Faça um programa que declare uma variável com a sua idade e imprima no console um teste dizendo se você pode ou não dirigir (apenas maiores de idade).
+
+*/  let idade = 17
+
+    if(idade >= 18){
+      console.log("eu posso dirigir, pois sou maior de idade")
+    } else {
+      console.log("eu nao posso dirigir, pois sou maior de idade")
+    }
 
 
 
 /*
 2. Agora pesquise no Google o horário atual no Vietnã. Declare uma variável do tipo string que contenha os valores "manhã", "tarde", "noite" ou "madrugada"
 com base no fuso horário do Vietnã. Utilize o bloco if/else para escrever uma mensagem, sendo ela "Bom dia, Vietnã!", "Boa tarde, Vietnã", "Boa noite, Vietã" ou "Boa madrugada, Vietnã", dependendo de qual for o seu fuso horário atual.
-*/
+*/  
+   let horarionovietna = "madrugada"
+  
+
+   if(horarionovietna.toUpperCase() === "manha"){
+    console.log('bom dia, vietna!')
+   } else if (horarionovietna.toUpperCase() === "TARDE") {
+    console.log("boa tarde, vietna!")
+   } else if (horarionovietna.toUpperCase() === "NOITE") {
+    console.log('boa noite, vieta!')
+   } else {
+    console.log("boa madrugada, vietna!")
+   }
 
 
 /*
 3. Agora repita o exercício anterior, porém utilizando o bloco Switch/Case e utilizando o fuso horário do Havaí.
 */
+ let horario = "manhã"
+
+ switch(horario.toUpperCase()){
+  case 'manhã':
+    console.log('bom dia, havai!')
+     break
+ case 'tarde':
+    console.log('boa tarde, havai!') 
+     break
+ case 'noite':
+    console.log('boa noite, havai!')
+     break
+ case 'madrugada':
+      console.log('boa madrugada, havai!')
+ }
+
+
+
+
